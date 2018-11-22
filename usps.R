dda27c47117fce692f8f7cdefba0c90c97d17f15
daboliang <steve.liang@f5tradingllc.com>
2018-11-21 23:00:45 -0600

    more sample addresses for testing; added vectorized functions to process addresses in batch

--- usps.R
+++ usps.R
@@ -1,3 +1,9 @@
+address_list <- data_frame(Street= c("520 N. Halsted", "43 Morris", "1299 ZUrich Way",
+                                     "565 W Madison", "62 North Ave", "35 Morris St North"),
+                           City = c("Chicago", "chicago", "Schaumburg",
+                                    "Chicago", "Chiago", "Park Ridge"),
+                           State = rep("IL", 6))
+
 username <- "996ZURIC4170"
 
 ROOT_URL <- "http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML="
@@ -6,10 +12,14 @@ request_template <- list(
   )
 )
 
-validate_address <- function(usr, street, city, state){
+validate_address <- function(usr, address_df){
+  dplyr::mutate(dplyr::rowwise(address_df), xml_url = compose_xml(usr, Street, City, State, row_number()))
+}
+
+compose_xml <- function(usr, street, city, state, row_index){
   xml_req <- xml2::as_xml_document(request_template)
-  xml2::xml_attr(xml_req, "USERID") <- username
-  address_node <- xml2::xml_add_child(xml_req, "Address", ID = "0")
+  xml2::xml_attr(xml_req, "USERID") <- usr
+  address_node <- xml2::xml_add_child(xml_req, "Address", ID = row_index %% 5)
   
   xml2::xml_add_child(address_node, "Address1")
   xml2::xml_add_child(address_node, "Address2", street)
