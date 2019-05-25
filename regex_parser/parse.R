448903f5d70d9478467e99bf55170f56f1d9ca77
steve-liang <steve.liang@f5tradingllc.com>
2019-05-25 16:03:16 +0000

    added option to remove suffix as the result

--- regex_parser/parse.R
+++ regex_parser/parse.R
@@ -1,5 +1,5 @@
 
-to_building_address <- function(x, removeStreetName = TRUE){
+to_building_address <- function(x, removeSuffix = TRUE){
   #### STEP 1, Suite/Unit Number #######
   #### thanks to https://stackoverflow.com/questions/51370415/remove-unit-number-clause-from-street-address-string-in-r
   # x <- stringr::str_replace_all(x, "#[[:digit:]]*", "")
@@ -14,8 +14,15 @@ to_building_address <- function(x, removeStreetName = TRUE){
   x <- stringr::str_replace_all(x, stringr::regex(dir_mapping, ignore_case = TRUE)) # standardize directions
   x <- stringr::str_replace_all(x, stringr::regex(nsn_mapping, ignore_case = TRUE)) # standardize numbered street name
   
+  if(removeSuffix){
+    x <- stringr::str_replace_all(x, stringr::regex(paste0("\\b",
+                                                           paste(collapse = "\\b|\\b", unname(suffix_mapping)),
+                                                           "\\b"), ignore_case = TRUE), "")
+  }
+  
   ### FINAL STEP: convert to upper case and remove extra spaces
   x <- stringr::str_squish(stringr::str_to_upper(x))
+  
   return(x)
 }
 
